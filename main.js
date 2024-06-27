63a6b3aaf5a5ed6f83583c7537d3c0a68669a8b1
Akorede <abeebadetunji203@gmail.com>
2024-06-27 10:52:28 -1100

    frequently ask question

---
+++ main.js
@@ -0,0 +1,94 @@
+// function start(){
+//     if(result.style.display ==="none") {
+//         result.style.display = "block"; document.getElementById("plus").innerHTML = "-" 
+//     } else  {result.style.display = "none"; document.getElementById("plus").innerHTML = "+" 
+        
+//     }
+    
+    
+// }
+
+
+
+function start() {
+var div = document.getElementById('question')
+var button = document.getElementById("plus")
+
+    if (div.style.height === "100px") {
+        div.style.height = "40px";
+        button.textContent = "+"
+    }else{
+        div.style.height = "100px";
+        button.textContent = "X";
+    }
+
+
+        
+}
+function Apple() {
+    var div = document.getElementById('questiontwo')
+    var button = document.getElementById("Qtwo")
+    
+        if (div.style.height === "100px") {
+            div.style.height = "40px";
+            button.textContent = "+"
+        }else{
+            div.style.height = "100px";
+            button.textContent = "x"
+        }
+    }
+
+    function Next() {
+        var div = document.getElementById('questionthree')
+        var button = document.getElementById("Qthree")
+        
+            if (div.style.height === "100px") {
+                div.style.height = "40px";
+                button.textContent = "+"
+            }else{
+                div.style.height = "100px";
+                button.textContent = "X"
+            }
+        }
+    
+        function Nigga() {
+            var div = document.getElementById('questionfour')
+            var button = document.getElementById("Qfour")
+            
+                if (div.style.height === "100px") {
+                    div.style.height = "40px";
+                    button.textContent = "+"
+                }else{
+                    div.style.height = "100px";
+                    button.textContent = "X"
+                }
+            }
+
+            function luiz() {
+                var div = document.getElementById('questionfive')
+                var button = document.getElementById("Qfive")
+                
+                    if (div.style.height === "100px") {
+                        div.style.height = "40px";
+                        button.textContent = "+"
+                    }else{
+                        div.style.height = "100px";
+                        button.textContent = "X"
+                    }
+                }
+
+                function nice() {
+                    var div = document.getElementById('questionsix')
+                    var button = document.getElementById("Qsix")
+                    
+                        if (div.style.height === "100px") {
+                            div.style.height = "40px";
+                            button.textContent = "+"
+                        }else{
+                            div.style.height = "100px";
+                            button.textContent = "X"
+                        }
+                    }
+
+    
+
